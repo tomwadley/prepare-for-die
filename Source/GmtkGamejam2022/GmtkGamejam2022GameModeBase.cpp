411ff8ec2d975158e118416be6925af1eed01666
Tom Wadley <tom@tomwadley.net>
2022-07-15 21:55:08 -0500

    Position camera over board

--- Source/GmtkGamejam2022/GmtkGamejam2022GameModeBase.cpp
+++ Source/GmtkGamejam2022/GmtkGamejam2022GameModeBase.cpp
@@ -4,13 +4,16 @@ void AGmtkGamejam2022GameModeBase::BeginPlay()
 {
 	Super::BeginPlay();
 
-	for (int i = 0; i < Rows; i++)
+	const double XOffset = (Columns - 1) * -ATile::HalfLength;
+	const double YOffset = (Rows - 1) * -ATile::HalfLength;
+
+	for (int i = 0; i < Columns; i++)
 	{
 		Tiles.Add(TArray<ATile*>());
-		for (int j = 0; j < Columns; j++)
+		for (int j = 0; j < Rows; j++)
 		{
-			const double X = i * ATile::Length;
-			const double Y = j * ATile::Length;
+			const double X = XOffset + i * ATile::Length;
+			const double Y = YOffset + j * ATile::Length;
 			const FVector Location = FVector(X, Y, 0.f);
 			ATile* NewTile = Cast<ATile>(GetWorld()->SpawnActor(EmptyTileClass, &Location));
 			Tiles[i].Add(NewTile);
@@ -22,6 +25,6 @@ void AGmtkGamejam2022GameModeBase::BeginPlay()
 
 void AGmtkGamejam2022GameModeBase::SpawnDie()
 {
-	const FVector DieLocation = FVector(200.f, 200.f, 400.f);
+	const FVector DieLocation = FVector(200.f, 200.f, 200.f);
 	Dies.Add(Cast<ADie>(GetWorld()->SpawnActor(DieClass, &DieLocation)));
 }
